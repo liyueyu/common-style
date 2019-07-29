e05771d1ead1a80fb1390384ad38cfdfaacc1dba
yueyu_li <yueyu_li@kingdee.com>
2019-07-29 15:56:13 +0800

    component: layout

--- src/pages/cssComponents/layout.vue
+++ src/pages/cssComponents/layout.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
+    <div class="layout-page">
+        <div class="content-header">
+            <h2>Layout <small class="text-secondary f-s-16">- 布局</small></h2>
+            <p class="f-s-14">包含：页面结构、flex 栅格</p>
+            <p class="f-s-14">对应文件 layout.less</p>
+        </div>
+        <h3>常见页面结构</h3>
         <div class="layout-wrap">
             <div class="ui-flex-grid vertical-center horizontally-stretch" style="height: 100%">
                 <div class="ui-flex-grid-none bg-gray-3">header</div>
@@ -68,6 +74,32 @@
                 <div class="ui-flex-grid-none bg-gray-7">footer</div>
             </div>
         </div>
+        <h3>flex 栅格 - <small class="text-secondary">平均分割（水平、垂直）</small></h3>
+        <div class="flex-grid-wrap">
+            <div class="ui-flex-grid">
+                <div class="ui-flex-grid-item" v-for="index in 2">二等分({{index}})</div>
+            </div>
+            <div class="ui-flex-grid">
+                <div class="ui-flex-grid-item" v-for="index in 3">三等分({{index}})</div>
+            </div>
+            <div class="ui-flex-grid">
+                <div class="ui-flex-grid-item" v-for="index in 5">五等分({{index}})</div>
+            </div>
+            <div class="ui-flex-grid">
+                <div class="ui-flex-grid-item" v-for="index in 8">八等分({{index}})</div>
+            </div>
+            <div class="ui-flex-grid">
+                <div class="ui-flex-grid-item" v-for="index in 12">十二等分({{index}})</div>
+            </div>
+        </div>
+        <div class="flex-grid-wrap">
+            <div class="ui-flex-grid vertical-center">
+                <div class="ui-flex-grid-item" v-for="index in 2">垂直二等分({{index}})</div>
+            </div>
+            <div class="ui-flex-grid vertical-center">
+                <div class="ui-flex-grid-item" v-for="index in 5">垂直五等分({{index}})</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -76,3 +108,19 @@
         name: 'layout'
     }
 </script>
+<style lang="less" scoped>
+    .flex-grid-wrap .ui-flex-grid{
+        text-align: center;
+        margin-bottom: 12px;
+        &.vertical-center {
+            height: 150px;
+        }
+        .ui-flex-grid-item {
+            padding: 4px ;
+            background-color: #e9ecf1;
+            &:nth-child(2n) {
+                background-color: #d8d8d8;
+            }
+        }
+    }
+</style>
